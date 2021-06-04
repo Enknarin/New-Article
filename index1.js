ff421c3d4b63f0f72ee885b7a939af82b25224b9
Enknarin <s5935512076@phuket.psu.ac.th>
2021-06-04 10:15:10 +0700

    latest update

---
+++ index1.js
@@ -0,0 +1,43 @@
+const db = firebase.firestore();
+const taskContainer = document.getElementById('task-container');
+const taskButton = document.getElementById('task-button');
+
+
+function myFunction(id) {
+    var docRef = db.collection('Travel').doc(id);
+    docRef.get().then((doc) => {
+    if(doc.exists){
+        console.log("Document Data", doc.data());
+        const task = doc.data();
+
+        taskContainer.innerHTML = `
+        <div> 
+            <h3>${task.topic}</h3>
+            <p>${task.blog}</p>
+        </div>`
+    }else{
+        console.log("No Document");
+    }
+    }).catch((error) => {
+        console.log("Error", error);
+    })
+
+  }
+
+db.collection("Travel").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        const task = doc.data();
+        task.id = doc.id;
+        taskButton.innerHTML += `<div>
+        <a class="btn-topic" data-id="${task.id}" href="#">${task.topic}</a>
+        </div>`
+
+        const btnTopic = document.querySelectorAll('.btn-topic');
+        btnTopic.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                myFunction(e.target.dataset.id)
+            })
+
+        })
+    });
+});
---
+++ index2.js
@@ -0,0 +1,45 @@
+const db = firebase.firestore();
+const taskContainer = document.getElementById('task-container');
+const taskButton = document.getElementById('task-button');
+
+
+function myFunction(id) {
+    var docRef = db.collection('Food').doc(id);
+    docRef.get().then((doc) => {
+    if(doc.exists){
+        console.log("Document Data", doc.data());
+        const task = doc.data();
+
+        taskContainer.innerHTML = `
+        <div> 
+            <h3>${task.topic}</h3>
+            <p>${task.blog}</p>
+        </div>`
+    }else{
+        console.log("No Document");
+    }
+    }).catch((error) => {
+        console.log("Error", error);
+    })
+
+  }
+
+db.collection("Food").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        const task = doc.data();
+        task.id = doc.id;
+        // console.log(doc.id, " => ", doc.data().topic);
+        taskButton.innerHTML += `<div>
+        <a class="btn-topic" data-id="${task.id}" href="#">${task.topic}</a>
+        </div>`
+
+        const btnTopic = document.querySelectorAll('.btn-topic');
+        btnTopic.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                // console.log(e.target.dataset.id);
+                myFunction(e.target.dataset.id)
+            })
+
+        })
+    });
+});
