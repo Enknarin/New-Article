d7a378a4f685ab7a662961ca84841923caa65c88
Enknarin <s5935512076@phuket.psu.ac.th>
2021-06-09 13:17:12 +0700

    test fiz 31

--- index4.js
+++ index4.js
@@ -11,10 +11,54 @@ function myFunction(id) {
             const task = doc.data();
 
             taskContainer.innerHTML = `
-        <div> 
-            <h3 class="Mh2" >${task.topic}</h3>
-            <img  class="picepost"  src= "${task.Link}" ></img>
-            <p>${task.blog}</p>
+            <div class="entry-content">
+             <h2 class="Mh2">${task.topic}</h2>
+            
+            
+            <div class="blog-content">
+           
+            <img    src= "${task.Link}" width="100%" class="p" ></img>
+                
+            <div class="entry-content">
+                    <p>${task.blog}</p>
+            
+             
+            
+            
+           
+
+
+            </div>
+            </div>
+
+            <div class="blog-content">
+            <h3 class="entry-title"></h3>
+
+            <div class="entry-content">
+
+                <div class="tags">
+                <h10 >© 2021 Cable Article Network.</10>
+                    <ul class="social">
+                    <li><a href="https://www.facebook.com/" target="_blank" title="Facebook"><i
+                    class="fa fa-facebook"></i></a></li>
+        <li><a href="https://twitter.com/" target="_blank" title="Twitter"><i
+                    class="fa fa-twitter"></i></a></li>
+        <li><a href="https://www.linkedin.com/" target="_blank" title="Linkedin"><i
+                    class="fa fa-linkedin"></i></a></li>
+        <li><a href="https://www.tumblr.com/" target="_blank" title="Tumblr"><i
+                    class="fa fa-tumblr"></i></a></li>
+        <li><a href="https://vimeo.com/" target="_blank" title="Vimeo"><i
+                    class="fa fa-vimeo"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+            </div>
         </div>`
         } else {
             console.log("No Document");
@@ -58,12 +102,14 @@ db.collection("Electrical").get().then((querySnapshot) => {
         <div class="type-post">        
             <div class="blog-content">
                 
-                <div class="entry-content">
-                <div>
-                <p>${task.topic}</p>  
-                <img src="${task.Link}" width="400 px">
-                <p>${task.des}</p>
-                <a class="btn-link" data-id="${task.id}" href="#">Read more</a>
+            <div class="entry-content">
+            <div>
+            <p  class="box">${task.topic}</p>  
+            <img src="${task.Link}"  width="100%" class="p" >
+            <p  class="box">${task.des}</p> 
+
+            <a class="btn-link" data-id="${task.id}" href="#">Read more</a>
+    
         
                 </div>
                  </div>
